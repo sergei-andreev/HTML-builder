c689eeb220d85fd8f4c389671915e41957ac500f
Sergei Andreev <sergey.andreev@dualbootpartners.com>
2023-05-08 20:26:28 +0200

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const {readdir, mkdir, copyFile, rmdir, stat} = require('fs').promises;
+const {join} = require('path');
 
+const copyDir = async (sourceFolder) => {
+    const distFolder = join(__dirname, 'files-copy');
+
+    try {
+        await rmdir(distFolder, {recursive: true});
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw err;
+        }
+    }
+
+    const files = await readdir(sourceFolder);
+
+    await mkdir(distFolder, {recursive: true});
+
+    for (const file of files) {
+        const sourceFile = join(sourceFolder, file);
+        const destFile = join(distFolder, file);
+        await copyFile(sourceFile, destFile);
+    }
+}
+
+const sourceFolder = join(__dirname, 'files');
+copyDir(sourceFolder);
